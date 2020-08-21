3b295cc6b0d122b842b466f3080cc2289e60042a
hsnkazmi <kazmi.namal@gmail.com>
2020-08-21 17:50:19 +0500

    defaulttabbarcontroller

--- ecardprofiler/lib/main.dart
+++ ecardprofiler/lib/main.dart
@@ -1,40 +1,65 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
 
-void main() => runApp(Statsapp());
-
-class Statsapp extends StatefulWidget {
-  Statsapp({Key key}) : super(key: key);
-
-  @override
-  _StatsappState createState() => _StatsappState();
+void main() {
+  runApp(TabBarDemo());
 }
 
-class _StatsappState extends State<Statsapp> {
-  int randomnumber = 1;
-  caluculus() {
-    setState(() {
-      randomnumber = Random().nextInt(9) + 1;
-    });
-  }
+class TabBarDemo extends StatelessWidget {
+  const TabBarDemo({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Appbar"),
-        ),
-        body: Center(
-          child: Text("$randomnumber"),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: caluculus(),
-          child: Icon(Icons.ac_unit),
-          backgroundColor: Colors.amberAccent,
-          tooltip: "tooltip",
-        ),
+    var ktabs = <Tab>[
+      Tab(
+        icon: Icon(Icons.sd_card),
       ),
+      Tab(
+        icon: Icon(Icons.ac_unit),
+      ),
+      Tab(
+        icon: Icon(Icons.ac_unit),
+      ),
+    ];
+    var ktabview = <Widget>[
+      Center(
+        child: Text("data"),
+      ),
+      Center(
+        child: Text("data"),
+      ),
+      Center(
+        child: Text("data"),
+      )
+    ];
+    return MaterialApp(
+      home: DefaultTabController(
+          length: ktabs.length,
+          child: Scaffold(
+            appBar: AppBar(
+              bottom: TabBar(tabs: ktabs), //tab widgets
+            ),
+            body: TabBarView(children: ktabview), //widgets
+          )),
     );
   }
 }
+
+/*  
+ one old way
+ home: DefaultTabController(
+          length: 3,
+          child: Scaffold(
+            appBar: AppBar(
+              bottom: TabBar(tabs: [
+                Text("1"),
+                Text("2"),
+                Text("3"),
+              ]),
+            ),
+            body: TabBarView(children: [
+              Text("1"),
+              Text("2"),
+              Text("3"),
+            ]),
+          )),
+    */
